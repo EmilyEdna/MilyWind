eaef17031be4f1c9dd7fbca03dc144fc36412471
EmilyEdna <847432003@qq.com>
2021-08-25 18:38:51 +0800

    aop

--- Mily.Wind.Extens/AOPUtity/InterceptorAttribute.cs
+++ Mily.Wind.Extens/AOPUtity/InterceptorAttribute.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using XExten.Advance.AopFramework.AopAttribute;
+using XExten.Advance.StaticFramework;
 
 namespace Mily.Wind.Extens.AOPUtity
 {
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-    public class InterceptorAttribute: InterceptorBaseAttribute
+    public class InterceptorAttribute : InterceptorBaseAttribute
     {
         public override object Invoke(object obj, string methodName, object[] parameters)
         {
-            return base.Invoke(obj, methodName, parameters);
+            return SyncStatic.TryCatch(() =>
+             {
+                 return base.Invoke(obj, methodName, parameters);
+             }, ex =>
+             {
+                 //写日志
+                 return null;
+             });
+
         }
     }
 }
