71f06049772e844dc3ac2253cbeb46f77ed7c260
EmilyEdna <847432003@qq.com>
2021-12-02 13:04:30 +0800

    升级框架

--- 公用基础层/Mily.Wind.Extens/AOPUtity/ActionsAttribute.cs
+++ 公用基础层/Mily.Wind.Extens/AOPUtity/ActionsAttribute.cs
@@ -6,13 +6,18 @@ using XExten.Advance.AopFramework.AopAttribute;
 
 namespace Mily.Wind.Extens.AOPUtity
 {
-    public class ActionsAttribute: AopBaseActionAttribute
+    public class ActionsAttribute : AopBaseActionAttribute
     {
-        public override void Before(string methodName, object[] parameters)
+        public ActionsAttribute() : base() { }
+
+
+        public ActionsAttribute(string Code) : base(Code) { }
+
+        public override void Before(string methodName, Type classInfo, object[] parameters)
         {
             //这里可以做权限或者数据库隔离
         }
-        public override object After(string methodName, object result)
+        public override object After(string methodName, Type classInfo, object result)
         {
             MilyLogOption.TraceNode = string.Empty;
             return result;
